d55e22db113d6362ecb441d4851373237624e58a
Mr Smithy x <CharltonSmith@outlook.com>
2023-06-18 21:39:02 -0400

    test

--- inputbuf.cc
+++ inputbuf.cc
@@ -34,8 +34,8 @@ char InputBuffer::GetChar(char& c)
         c = input_buffer.back();
         input_buffer.pop_back();
     } else {
-        cin.clear();
         cin.get(c);
+        cin.clear();
     }
     return c;
 }
